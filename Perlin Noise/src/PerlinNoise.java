08c22b0f6b5da8a22099ffefe407cd69b6ba3ddb
smalls12 <cdriehuys@gmail.com>
2014-03-28 22:23:25 -0400

    Finished implementing 2D noise

--- Perlin Noise/src/PerlinNoise.java
+++ Perlin Noise/src/PerlinNoise.java
@@ -4,27 +4,35 @@ import java.util.Arrays;
 public class PerlinNoise {
 	
 	public final int octaves = 8;
-	public final float persistence = .25f;
+	public final float persistence = .5f;
 	
 	private int[][] primes;
 	private PrimeGenerator primeGen;
 	
 	public PerlinNoise(long seed) {
 		
-		primes = new int[octaves][3];
+		primes = new int[octaves][4];
 		primeGen = new PrimeGenerator(seed);
 		
 		for (int i = 0; i < primes.length; i++) {
-			primes[i][0] = primeGen.nextPrimeInRange(12000,  18000);
-			primes[i][1] = primeGen.nextPrimeInRange(750000, 800000);
-			primes[i][2] = primeGen.nextPrimeInRange(1350000000, 1400000000);
+			primes[i][0] = primeGen.nextPrimeInRange(45, 65);
+			primes[i][1] = primeGen.nextPrimeInRange(12000,  18000);
+			primes[i][2] = primeGen.nextPrimeInRange(750000, 800000);
+			primes[i][3] = primeGen.nextPrimeInRange(1350000000, 1400000000);
 		}
 	}
 	
 	public float noise1D(int x, int octave) {
 		
 		x = (x<<13) ^ x;
-		float val = (1f - ((x * (x * x * primes[octave][0] + primes[octave][1]) + primes[octave][2]) & 0x7FFFFFFF) / 1073741824f);
+		float val = (1f - ((x * (x * x * primes[octave][1] + primes[octave][2]) + primes[octave][3]) & 0x7FFFFFFF) / 1073741824f);
+		return val;
+	}
+	
+	public float noise2D(int x, int y, int octave) {
+		
+		int n = x + y * primes[octave][0];
+		float val = (1f - ((n * (n * n * primes[octave][1] + primes[octave][2]) + primes[octave][3]) & 0x7FFFFFFF) / 1073741824f);
 		return val;
 	}
 	
@@ -32,6 +40,15 @@ public class PerlinNoise {
 		
 		//online method
 		return noise1D(x, octave) / 2 + noise1D(x - 1, octave) / 4 + noise1D(x + 1, octave) / 4;
+	}
+	
+	public float smoothedNoise2D(int x, int y, int octave) {
+		
+		float corners = (noise2D(x - 1, y - 1, octave) + noise2D(x + 1, y - 1, octave) + noise2D(x - 1, y + 1, octave) + noise2D(x + 1, y + 1, octave)) / 16;
+		float sides = (noise2D(x - 1, y, octave) + noise2D(x + 1, y, octave) + noise2D(x, y - 1, octave) + noise2D(x, y + 1, octave)) / 8;
+		float center = noise2D(x, y, octave) / 4;
+		
+		return corners + sides + center;
 	}
 	
 	public float cosInterpolate(float a, float b, float x) {
@@ -53,6 +70,27 @@ public class PerlinNoise {
 		return cosInterpolate(v1, v2, fracX);
 	}
 	
+	public float interpolatedNoise2D(float x, float y, int octave) {
+		
+		int intX = (int) x;
+		float fracX = x - intX;
+		
+		int intY = (int) y;
+		float fracY = y - intY;
+		
+		float v1, v2, v3, v4, i1, i2;
+		
+		v1 = smoothedNoise2D(intX, intY, octave);
+		v2 = smoothedNoise2D(intX + 1, intY, octave);
+		v3 = smoothedNoise2D(intX, intY + 1, octave);
+		v4 = smoothedNoise2D(intX + 1, intY + 1, octave);
+		
+		i1 = cosInterpolate(v1, v2, fracX);
+		i2 = cosInterpolate(v3, v4, fracX);
+		
+		return cosInterpolate(i1, i2, fracY);
+	}
+	
 	public float perlinNoise1D(float x) {
 		
 		float total = 0;
@@ -63,6 +101,21 @@ public class PerlinNoise {
 			float amplitude = (float) Math.pow(persistence, i);
 			
 			total += interpolatedNoise1D(x * frequency, i) * amplitude;
+		}
+		
+		return total;
+	}
+	
+	public float perlinNoise2D(float x, float y) {
+		
+		float total = 0;
+		
+		for (int i = 0; i < octaves; i++) {
+			
+			int frequency = (int) Math.pow(2, i);
+			float amplitude = (float) Math.pow(persistence, i);
+			
+			total += interpolatedNoise2D(x * frequency, y * frequency, i) * amplitude;
 		}
 		
 		return total;
--- Perlin Noise/src/PerlinNoiseImage.java
+++ Perlin Noise/src/PerlinNoiseImage.java
@@ -1,5 +1,6 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.util.Random;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -8,7 +9,7 @@ import javax.swing.SwingUtilities;
 
 public class PerlinNoiseImage extends JFrame {
 	
-	private final long seed = 1343;
+	private final long seed = 2;
 	
 	public PerlinNoiseImage() {
 		
@@ -16,7 +17,7 @@ public class PerlinNoiseImage extends JFrame {
 		add(myImg);
 		myImg.repaint();
 		
-		setSize(640, 480);
+		setSize(1080, 720);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setResizable(false);
@@ -27,20 +28,40 @@ public class PerlinNoiseImage extends JFrame {
 		
 		private PerlinNoise noise = new PerlinNoise(seed);
 		
-		public void draw(Graphics g) {
+		public void draw1D(Graphics g) {
 			
-			for (int i = 0; i < 640; i++) {
+			for (int i = 0; i < 1080; i++) {
 				int cVal = Math.abs(Math.round(255 * noise.perlinNoise1D((float)i / 10)));
-				System.out.println("Noise value: " + noise.perlinNoise1D((float) i / 10) + ", cVal: " + cVal);
 				g.setColor(new Color(cVal, cVal, cVal));
-				g.fillRect(i, 0, 1, 470);
+				g.fillRect(i, 0, 1, 710);
+			}
+		}
+		
+		public void draw2D(Graphics g) {
+			
+			Random random = new Random();
+			
+			for (int i = 0; i < 1080; i++) {
+				for (int j = 0; j < 720; j++) {
+					float val = Math.abs(noise.perlinNoise2D((float) i / 100, (float) j / 100));
+					if (val < .1) {
+						g.setColor(new Color(0, 0, 175));
+					} else if (.1 <= val && val < .15) {
+						g.setColor(new Color(255, 255, 153));
+					} else if (.15 <= val && val < .35) {
+						g.setColor(new Color(0, 175, 0));
+					} else {
+						g.setColor(new Color(51, 153, 0));
+					}
+					g.fillRect(i, j, 1, 1);
+				}
 			}
 		}
 		
 		public void paint(Graphics g) {
 			super.paint(g);
 			
-			draw(g);
+			draw2D(g);
 		}
 	}
 
